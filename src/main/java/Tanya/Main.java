790a6608c26400f186b3d5a0e2ae1c0b579f70b8
Tanv7 <tanv7@yandex.ru>
2024-04-17 13:14:07 +0300

    Initial commit

---
+++ src/main/java/Tanya/Main.java
@@ -0,0 +1,34 @@
+package Tanya;
+
+public class Main {
+    public abstract class Zverushka{
+        public String Name;
+        public abstract void runAway();
+
+        public abstract void talk();
+    }
+
+}
+public class void getScared();
+System.out.println( "Напуганная зверюшка что-то говорит и убегает");
+
+
+public class Mouse extends Zverushka {
+    public class Cat extends Zverushka implements INoise {
+        System.out.println( "Кошка пугает мышку")
+    }
+        public class Dog extends Zverushka implements INoise {
+        System.out.println (" Собака пугает кошку ")
+
+}
+
+    @Override
+    public void noise() {
+        System.out.println("Зверушка шумит");
+    }
+}
+public interface Noisy {
+void noise (String n);}
+
+
+
